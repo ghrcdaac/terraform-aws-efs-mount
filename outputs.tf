9134048767e825c1b5e8f0aae450983144065862
Michael Hall <mlh0079@uah.edu>
2023-03-02 12:10:57 -0600

    - Allowing configuration for most of the efs fields.

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,12 @@
 output "file_system_dns_name" {
-  value = aws_efs_file_system.this.dns_name
+  value = aws_efs_file_system.file_system.dns_name
 }
 output "file_system_arn" {
-  value = aws_efs_file_system.this.arn
+  value = aws_efs_file_system.file_system.arn
 }
 
 output "file_system_id" {
-  value = aws_efs_file_system.this.id
+  value = aws_efs_file_system.file_system.id
 }
 
 output "ec2_security_group_id" {
@@ -15,5 +15,5 @@ output "ec2_security_group_id" {
 
 output "mount_target_ids" {
 
-  value =  [for mount_target in  aws_efs_mount_target.this: mount_target.id ]
+  value =  [for mount_target in  aws_efs_mount_target.mount_target: mount_target.id ]
 }
